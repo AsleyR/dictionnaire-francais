5fe114493b1ff234ab828e89579d7d9b84bd84e0
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-13 01:01:31 -0400

    Add simple footer

---
+++ client/app/(components)/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className='bg-[#54595D] py-[1rem] text-gray-50 grid justify-center drop-shadow-lg'>
+            <p className=''>Site web developpé par <a className='text-blue-400 hover:underline duration-200' target={'_blank'} href='https://asleyrobleto.com'>Asley Robleto</a>. 2024.</p>
+        </footer>
+    )
+}
+
+export default Footer
