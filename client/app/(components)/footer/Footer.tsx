a82e79da4d9f47b78afbaa33404d4faa62bd1544
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-15 00:14:57 -0400

    Add repo info to footer

--- client/app/(components)/footer/Footer.tsx
+++ client/app/(components)/footer/Footer.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className='bg-[#54595D] py-[1rem] text-gray-50 grid justify-center drop-shadow-lg'>
+        <footer className='bg-[#54595D] py-[1rem] text-center text-gray-50 grid justify-center drop-shadow-lg'>
             <p className=''>Site web developpé par <a className='text-blue-400 hover:underline duration-200' target={'_blank'} href='https://asleyrobleto.com'>Asley Robleto</a>. 2024.</p>
+            <p>Voir le <a href='https://github.com/AsleyR/dictionnaire-francais' target={'_blank'} className='text-blue-400 hover:underline duration-200'>repo</a> du projet.</p>
         </footer>
     )
 }
