8cff814faaf901f2dcf0379bf54aea7cf58074ae
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-14 22:45:10 -0400

    Add VerbeInfoBlockContainer component

---
+++ client/app/(components)/word/VerbeInfoBlockContainer.tsx
@@ -0,0 +1,12 @@
+export default function VerbeInfoBlockContainer({ header, body }: { header: string, body: string | null }) {
+    return (
+        <div className="grid grid-rows-2 border border-gray-200 rounded text-xl shadow-sm">
+            <div className="p-5">
+                {header}
+            </div>
+            <div className={`font-bold border-t border-gray-200 p-5 ${!body ? "bg-gray-100" : ""}`}>
+                {body}
+            </div>
+        </div>
+    )
+}
