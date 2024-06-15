172b3ec1a88f37c149ce6fc414b9bc3bb76a71c8
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-14 22:44:58 -0400

    Add RandomVerbeBtn component

---
+++ client/app/(components)/word/RandomVerbeBtn.tsx
@@ -0,0 +1,14 @@
+import getRandomVerbe from '@/app/(actions)/verbes/getRandomVerbe'
+import React from 'react'
+
+export default async function RandomVerbeBtn() {
+    const randomVerbe = await getRandomVerbe().then((verbe) => verbe.verbe)
+
+    return (
+        <a href={`/dictionary/fr/${randomVerbe}`}
+            className='w-fit border border-gray-300 hover:border-transparent hover:bg-black/60 hover:text-white duration-200 rounded shadow-sm p-3'
+        >
+            <p>J'ai de la chance</p>
+        </a>
+    )
+}
