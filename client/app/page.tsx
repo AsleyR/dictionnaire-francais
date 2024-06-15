6338b8ab97777b5f8488d869aaa7f39d249b2e91
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-14 22:42:13 -0400

    Add search bar and a I'm feeling lucky btn

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,17 @@
+import SearchBar from "./(components)/searchBar/SearchBar";
+import RandomVerbeBtn from "./(components)/word/RandomVerbeBtn";
 
 export default function Home() {
   return (
-    <main className="px-mobilex md:px-normalx pt-[3rem]">
-      <div className="flex flex-col space-y-3">
-        <h1 className="font-bold text-3xl">Bienvenue à votre source d'information le plus important!</h1>
-        <p className="text-xl">Le Dictionnaire en Français le plus faible de tous.</p>
+    <main className="m-auto my-20 px-mobilex md:px-normalx pt-[3rem]">
+      <div className="grid grid-auto gap-16 max-w-[60rem]">
+        <h1 className="font-bold text-2xl md:text-4xl text-center">Bienvenue à votre dictionnaire de prépositions préféré!</h1>
+        <div className="flex flex-col gap-3">
+          <SearchBar />
+          <div className="flex justify-center">
+            <RandomVerbeBtn />
+          </div>
+        </div>
       </div>
     </main>
   );
