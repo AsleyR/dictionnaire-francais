9ceb9215d3c03fa0707d3a51af2866337e4e32f5
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-15 00:16:31 -0400

    Add logic to replace qqn & qqch words

--- client/app/(components)/word/WordUtilisation.tsx
+++ client/app/(components)/word/WordUtilisation.tsx
@@ -1,29 +1,33 @@
 import { Word } from "@/app/(libs)/@types/index.t"
 import capitalizeFirstLetter from "@/app/(libs)/capitalizeFirstLetter"
+import replaceQqnAndQqch from "@/app/(libs)/replaceQqnAndQqch"
 
 export default function WordUtilisation({ word }: { word: Word }) {
     const capitalizedVerbe = capitalizeFirstLetter(decodeURIComponent(word.verbe))
 
-    function wordExamplesString(word: Word, capitalize: boolean = true, vi: boolean = false): string {
+    function wordExamplesString(word: Word, vi: boolean = false, capitalize: boolean = true): string {
         let constructedWord = `${word.verbe}`
-
-        // Assuming word.vi is not null
-        if (vi) {
-            constructedWord = `${constructedWord} ${word.vi}`
-
-            return constructedWord
-        }
 
         if (capitalize) {
             constructedWord = capitalizeFirstLetter(word.verbe)
         }
 
+        // Assuming word.vi is not null
+        if (word.vi && vi) {
+            const replaceQqchAndQqchWord = replaceQqnAndQqch(word.vi)
+            constructedWord = `${constructedWord} ${replaceQqchAndQqchWord}`
+
+            return constructedWord
+        }
+
         if (word.qqch) {
-            constructedWord = `${constructedWord} ${word.qqch}`
+            const replaceQqchWord = replaceQqnAndQqch(word.qqch)
+            constructedWord = `${constructedWord} ${replaceQqchWord}`
         }
 
         if (word.qqn) {
-            constructedWord = `${constructedWord} ${word.qqn}`
+            const replaceQqnhWord = replaceQqnAndQqch(word.qqn)
+            constructedWord = `${constructedWord} ${replaceQqnhWord}`
         }
 
         return constructedWord
@@ -35,11 +39,11 @@ export default function WordUtilisation({ word }: { word: Word }) {
             <div className="flex flex-col gap-2 p-5 border border-gray-200 shadow-sm rounded">
                 {
                     word.qqch || word.qqn ?
-                        <p>{wordExamplesString(word)}</p>
+                        <p>{wordExamplesString(word, false)}</p>
                         : null
                 }
                 {
-                    word.vi ? <p>{`${capitalizedVerbe} ${word.vi}`}</p> : null
+                    word.vi ? <p>{`${wordExamplesString(word, true)}`}</p> : null
                 }
             </div>
         </div>
