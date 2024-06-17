465699af0c48108612fd7438c0b331a30a76ef1f
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-17 07:08:14 -0400

    Update footer css to make it bluer

--- client/app/(components)/footer/Footer.tsx
+++ client/app/(components)/footer/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className='bg-gray-100 py-[1rem] border-t border-gray-300 text-center grid justify-center shadow-lg'>
-            <p className=''>Site web developpé par <a className='text-blue-500 hover:underline duration-200' target={'_blank'} href='https://asleyrobleto.com'>Asley Robleto</a>. 2024.</p>
-            <p>Voir le <a href='https://github.com/AsleyR/dictionnaire-francais' target={'_blank'} className='text-blue-500 hover:underline duration-200'>repo</a> du projet.</p>
+        <footer className='bg-blue-900 text-gray-100 py-[1rem] border-t border-gray-300 text-center grid justify-center shadow-lg'>
+            <p className=''>Site web developpé par <a className='text-purple-300 hover:underline duration-200' target={'_blank'} href='https://asleyrobleto.com'>Asley Robleto</a>. 2024.</p>
+            <p>Voir le <a href='https://github.com/AsleyR/dictionnaire-francais' target={'_blank'} className='text-purple-300 hover:underline duration-200'>repo</a> du projet.</p>
         </footer>
     )
 }
