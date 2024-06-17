e3969be5d51297e37e391e68270202dbcd2a5dc8
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-16 20:30:22 -0400

    Change links text color to a little darker blue

--- client/app/(components)/footer/Footer.tsx
+++ client/app/(components)/footer/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Footer() {
     return (
         <footer className='bg-gray-100 py-[1rem] border-t border-gray-300 text-center grid justify-center shadow-lg'>
-            <p className=''>Site web developpé par <a className='text-blue-400 hover:underline duration-200' target={'_blank'} href='https://asleyrobleto.com'>Asley Robleto</a>. 2024.</p>
-            <p>Voir le <a href='https://github.com/AsleyR/dictionnaire-francais' target={'_blank'} className='text-blue-400 hover:underline duration-200'>repo</a> du projet.</p>
+            <p className=''>Site web developpé par <a className='text-blue-500 hover:underline duration-200' target={'_blank'} href='https://asleyrobleto.com'>Asley Robleto</a>. 2024.</p>
+            <p>Voir le <a href='https://github.com/AsleyR/dictionnaire-francais' target={'_blank'} className='text-blue-500 hover:underline duration-200'>repo</a> du projet.</p>
         </footer>
     )
 }
