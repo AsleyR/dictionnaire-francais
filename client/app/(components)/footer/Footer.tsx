ccfc167472f5e1b07159826fcdab3d8a91ab2e18
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-16 20:27:36 -0400

    Change background color and text color

--- client/app/(components)/footer/Footer.tsx
+++ client/app/(components)/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className='bg-[#54595D] py-[1rem] text-center text-gray-50 grid justify-center drop-shadow-lg'>
+        <footer className='bg-gray-100 py-[1rem] border-t border-gray-300 text-center grid justify-center shadow-lg'>
             <p className=''>Site web developpé par <a className='text-blue-400 hover:underline duration-200' target={'_blank'} href='https://asleyrobleto.com'>Asley Robleto</a>. 2024.</p>
             <p>Voir le <a href='https://github.com/AsleyR/dictionnaire-francais' target={'_blank'} className='text-blue-400 hover:underline duration-200'>repo</a> du projet.</p>
         </footer>
