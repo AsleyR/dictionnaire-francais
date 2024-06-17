c6914c317b968b83ae5ef6dd70d780d3826cbfaa
AsleyR <79556058+AsleyR@users.noreply.github.com>
2024-06-17 07:04:55 -0400

    Implemented Logo component

--- client/app/(components)/navbar/Navbar.tsx
+++ client/app/(components)/navbar/Navbar.tsx
@@ -2,15 +2,13 @@ import Link from 'next/link'
 import React from 'react'
 import NavbarSearch from './NavbarSearch'
 import MobileNavbarSearch from './MobileNavbarSearch'
+import Logo from './Logo'
 
 function Navbar() {
     return (
         <nav className='sticky z-10 top-0 grid grid-cols-2 h-[5rem] px-mobilex md:px-normalx items-center drop-shadow-sm border-b border-gray-300 bg-[#f8f9fa] text-gray-900'>
             <div className="w-fit">
-                <Link href={'/'} className='flex'>
-                    <h2 className='hidden md:block font-bold text-2xl'>Dictionnaire Français</h2>
-                    <h2 className='block md:hidden font-bold text-3xl'>DF</h2>
-                </Link>
+                <Logo />
             </div>
             <div className="">
                 <ul className='flex space-x-5 md:space-x-7 items-center justify-end text-lg'>
